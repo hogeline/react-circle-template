af9816c7496282ce730a82688701cca78bbf0384
hogetaro <hoge@hoge.com>
2018-10-06 20:55:10 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import * as serviceWorker from "./utils/serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.unregister();
